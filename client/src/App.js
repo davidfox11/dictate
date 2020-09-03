ca1aae7da2268e35358808b87ac20fb7550b5955
Roisin Fox <ddtrifox@gmail.com>
2020-09-03 15:40:19 +0200

    fixed client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Body from './Body';
+import Head from './Head';
+
+export default () => {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+};
